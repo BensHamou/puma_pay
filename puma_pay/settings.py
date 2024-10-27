312f7695fe5ca69b5422753a53ba149f8c775698
mohammed.benslimane <benshamou@gmail.com>
2024-10-27 09:35:10 +0100

    update mailing account

--- puma_pay/settings.py
+++ puma_pay/settings.py
@@ -153,8 +153,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pumatransplaninng@gmail.com'
-EMAIL_HOST_PASSWORD = 'hvywqabtuhfipwnq'
+EMAIL_HOST_USER = 'pumapaiement@gmail.com'
+EMAIL_HOST_PASSWORD = 'ctybajcnepviiqah'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'pumatransplaninng@gmail.com'
+DEFAULT_FROM_EMAIL = 'pumapaiement@gmail.com'
 
